edd177814ebdbdca40bacc1cd829e98ba37c78e7
Dvir Volk <dvirsky@gmail.com>
2015-03-10 14:09:32 +0200

    read abs path for requirements

--- setup.py
+++ setup.py
@@ -4,6 +4,14 @@ import os
 
 version = 0, 1
 
+def read_requirements():
+
+    name = os.path.join(os.path.dirname(os.path.abspath(__file__)),'requirements.txt')
+    
+    with open(name) as fp:
+        return filter(None, fp.read().splitlines())
+
+
 setup(
     name='meduza',
     description = 'A python library for meduza',
@@ -12,7 +20,7 @@ setup(
     author_email='dvir@everything.me',
     url='https://gitlab.doit9.com/backend/meduza-py',
     packages=find_packages(),
-    install_requires=filter(None, open('requirements.txt').read().splitlines())
+    install_requires=read_requirements()
 
 )
 
