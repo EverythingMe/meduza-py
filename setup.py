0eb13d79e3039d592304a13031b91d764fa3aa9f
Roey Berman <roey@everything.me>
2015-04-12 14:20:35 +0300

    setup.py use gen_version.py from buildutils

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
-import os
+from os import path
 
-version = 0, 1
+pwd = lambda f: path.join(path.abspath(path.dirname(__file__)), f)
+contents = lambda f: open(pwd(f)).read().strip()
 
 
 setup(
     name='meduza',
     description = 'A python library for meduza',
-    version='{0[0]}.{0[1]}.{1}'.format(version, os.getenv('BUILD_NUMBER', 0)),
+    version=contents('VERSION'),
     author='DoAT Media LTD.',
     author_email='dvir@everything.me',
     url='https://gitlab.doit9.com/backend/meduza-py',
